e2f1844b8b510c12a8639a993bc1fc9792586306
Grant Ridder <grant@teambanjo.com>
2016-11-20 21:39:50 -0800

    Remove mentions of 10gen and 10gen repo recipe
    10gen repo recipe was previously deprecated

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,6 @@ long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '0.16.3'
 
 recipe 'mongodb', 'Installs and configures a single node mongodb instance'
-recipe 'mongodb::10gen_repo', 'Adds the 10gen repo to get the latest packages'
 recipe 'mongodb::mongos', 'Installs and configures a mongos which can be used in a sharded setup'
 recipe 'mongodb::configserver', 'Installs and configures a configserver for mongodb sharding'
 recipe 'mongodb::shard', 'Installs and configures a single shard'
--- recipes/install.rb
+++ recipes/install.rb
@@ -1,5 +1,5 @@
 # install the mongodb_org_repo if necessary
-include_recipe 'mongodb::mongodb_org_repo' if %w(10gen mongodb-org).include?(node['mongodb']['install_method'])
+include_recipe 'mongodb::mongodb_org_repo' if node['mongodb']['install_method'] == 'mongodb-org'
 
 # prevent-install defaults, but don't overwrite
 file node['mongodb']['sysconfig_file'] do
